f9df67750bce677716d11a61ca4ac967f77ea7c2
admin <admin@admins-iMac-45.local>
2018-04-30 06:11:48 +0800

    first commit

---
+++ app/controllers/ads_controller.rb
@@ -0,0 +1,43 @@
+class AdsController < ApplicationController
+  def index
+    @all_ads = Ad.all
+  end
+  
+  def show
+      @ad = Ad.find(params[:id])
+  end
+  
+  def new
+      @ad = Ad.new
+      
+  end
+  
+  def create
+     @ad = Ad.new
+     @ad.name = params[:ad][:name]
+     @ad.price = params[:ad][:price]
+     
+     
+     @ad.save
+     
+     redirect_to "/ads/#{@ad.id}"
+     
+  end
+  
+  def delete
+      @ad = Ad.find(params[:id])
+      @ad.destroy
+      
+      redirect_to "/ads/"
+      
+  end
+  
+  def edit
+      @ad = Ad.find(params[:id])
+  end
+  
+  def update
+
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'ads' => 'ads#index'
+  get 'ads/new' => 'ads#new'
+  get 'ads/:id' => 'ads#show'
+  post 'ads' => 'ads#create'
+  get 'ads/:id/delete' => 'ads#delete'
+  get 'ads/:id/edit' => 'ads#edit'
+  get 'ads/:id/edit' => 'ads#update'
+  patch 'ads/:id/edit' => 'ads#update'
+end
